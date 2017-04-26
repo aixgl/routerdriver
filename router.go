7e2a5f4762094d5a5bdfadf9749fbcb30bcd28ce
slclub@163.com <slclub@163.com>
2017-04-26 19:00:27 +0800

    router added

---
+++ router.go
@@ -0,0 +1,156 @@
+package routerdriver
+
+import (
+	"net/http"
+)
+
+type IParams interface {
+	ByName(string) (string, bool)
+	By(uint) (string, bool)
+}
+
+type Router struct {
+	UrlMap           *UrlMap
+	NotFound         http.Handler
+	MethodNotAllowed http.Handler
+	PanicHandler     func(http.ResponseWriter, *http.Request, interface{})
+	HandleOPTIONS    bool
+}
+
+func New() *Router {
+	return &Router{
+		HandleOPTIONS: true,
+		UrlMap:        &UrlMap{Store: make(map[string][]*node), Num: 0},
+	}
+}
+
+// GET is a shortcut for router.Handle("GET", path, handle)
+func (r *Router) GET(path string, handle interface{}) {
+	r.Handle("GET", path, handle)
+}
+
+// HEAD is a shortcut for router.Handle("HEAD", path, handle)
+func (r *Router) HEAD(path string, handle interface{}) {
+	r.Handle("HEAD", path, handle)
+}
+
+// OPTIONS is a shortcut for router.Handle("OPTIONS", path, handle)
+func (r *Router) OPTIONS(path string, handle interface{}) {
+	r.Handle("OPTIONS", path, handle)
+}
+
+// POST is a shortcut for router.Handle("POST", path, handle)
+func (r *Router) POST(path string, handle interface{}) {
+	r.Handle("POST", path, handle)
+}
+
+// PUT is a shortcut for router.Handle("PUT", path, handle)
+func (r *Router) PUT(path string, handle interface{}) {
+	r.Handle("PUT", path, handle)
+}
+
+// PATCH is a shortcut for router.Handle("PATCH", path, handle)
+func (r *Router) PATCH(path string, handle interface{}) {
+	r.Handle("PATCH", path, handle)
+}
+
+// DELETE is a shortcut for router.Handle("DELETE", path, handle)
+func (r *Router) DELETE(path string, handle interface{}) {
+	r.Handle("DELETE", path, handle)
+}
+
+func (r *Router) Handle(method string, path string, handle interface{}) {
+	if path[0] != '/' {
+		panic("path must begin with '/' in path.your defined path is: " + path)
+	}
+
+	container := r.UrlMap
+	container.addRouter(path, handle, method)
+}
+
+func (r *Router) Handler(method, path string, handler http.Handler) {
+	r.Handle(method, path,
+		func(w http.ResponseWriter, req *http.Request) {
+			handler.ServeHTTP(w, req)
+		},
+	)
+}
+
+// ServeFiles serves files from the given file system root.
+// The path must end with "/*filepath", files are then served from the local
+// path /defined/root/dir/*filepath.
+// For example if root is "/etc" and *filepath is "passwd", the local file
+// "/etc/passwd" would be served.
+// Internally a http.FileServer is used, therefore http.NotFound is used instead
+// of the Router's NotFound handler.
+// To use the operating system's file system implementation,
+// use http.Dir:
+//     router.ServeFiles("/src/*filepath", http.Dir("/var/www"))
+func (r *Router) ServeFiles(path string, root http.FileSystem) {
+	if len(path) < 10 || path[len(path)-10:] != "/*filepath" {
+		panic("path must end with /*filepath in path '" + path + "'")
+	}
+
+	fileServer := http.FileServer(root)
+
+	r.GET(path, func(w http.ResponseWriter, req *http.Request, ps IParams) {
+		req.URL.Path, _ = ps.ByName("filepath")
+		fileServer.ServeHTTP(w, req)
+	})
+}
+
+func (r *Router) Lookup(method, path string) *RouterRet {
+	ret := r.UrlMap.getValue(path)
+
+	if ret.Type == "" || ret.Type == method {
+		return ret
+	}
+
+	return nil
+}
+
+//painc recover
+func (r *Router) recv(w http.ResponseWriter, req *http.Request) {
+	if rcv := recover(); rcv != nil {
+		r.PanicHandler(w, req, rcv)
+	}
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if r.PanicHandler != nil {
+		defer r.recv(w, req)
+	}
+
+	path := req.URL.Path
+	if pnode := r.UrlMap.getValue(path, req.Method); pnode != nil {
+		handle, ok := pnode.Handle.(func(http.ResponseWriter, *http.Request, IParams))
+		if ok {
+			handle(w, req, pnode)
+		} else if req.Method != "CONNECT" && path != "/" {
+			code := 301 // Permanent redirect, request with GET method
+			if req.Method != "GET" {
+				// Temporary redirect, request with same method
+				// As of Go 1.3, Go does not support status code 308.
+				code = 307
+			}
+
+			http.Redirect(w, req, req.URL.String(), code)
+			return
+		}
+	}
+
+	//option
+	if req.Method == "OPTIONS" {
+		// Handle OPTIONS requests
+		if r.HandleOPTIONS {
+			w.Header().Set("Allow", "GET,POST,PUT,DELETE")
+			return
+		}
+	}
+	// Handle 404
+	if r.NotFound != nil {
+		r.NotFound.ServeHTTP(w, req)
+	} else {
+		http.NotFound(w, req)
+	}
+}
--- urlmap.go
+++ urlmap.go
@@ -11,7 +11,7 @@ var Print = fmt.Println
 // Store define drouter
 type UrlMap struct {
 	Store map[string][]*node
-	num   uint
+	Num   uint
 }
 
 //defined router node that stored detail
@@ -35,7 +35,8 @@ type RouterRet struct {
 
 //add defined router
 func (um *UrlMap) addRouter(path string, handle interface{}, method string) {
-	pathArr := strings.Split(path, ":")
+	path = CleanPath(path)
+	pathArr := strings.Split(path, "*")
 
 	al := len(pathArr)
 	if al == 0 {
@@ -43,28 +44,37 @@ func (um *UrlMap) addRouter(path string, handle interface{}, method string) {
 	}
 
 	entityPath := pathArr[0]
-	count := 0
+	pathArr1 := strings.Split(entityPath, ":")
+	entityPath = pathArr1[0]
+	pathArr1 = append(pathArr1, pathArr[1:]...)
+	count := 1
 	if um.Store[entityPath] == nil {
 		um.Store[entityPath] = make([]*node, count)
 	}
 
 	nod := &node{
 		path:       entityPath,
-		ParamSlice: nil,
+		ParamSlice: pathArr1[1:],
+		ParamMap:   make(map[string]uint),
 		Handle:     handle,
 		Type:       method,
 	}
 
+	for inx, val := range pathArr1[1:] {
+		nod.ParamMap[val] = uint(inx)
+	}
+
+	if can, err := um.exists(entityPath, nod); can {
+		panic(err.Error())
+	}
+
 	switch al {
 	case 1:
-		if can, err := um.exists(entityPath, nod); can {
-			panic(err.Error())
-		}
 	case 2:
 	}
 
 	um.Store[entityPath] = append(um.Store[entityPath], nod)
-	um.num++
+	um.Num++
 	return
 }
 
@@ -88,14 +98,19 @@ func (um *UrlMap) exists(real string, node *node) (bool, error) {
 
 //get return data based on the requested route
 //return
-func (um *UrlMap) getValue(reqPath string) *RouterRet {
+func (um *UrlMap) getValue(reqPath string, margs ...string) *RouterRet {
 
 	rpLen := len(reqPath)
 	var nods []*node
-	var key string
+	var key, method string
 	var values []string = make([]string, 0)
 	var jk = rpLen - 1
 
+	if len(margs) >= 1 {
+		method = margs[0]
+	}
+
+	//Reversal traversal url
 	for i := rpLen - 1; i >= 0; i-- {
 		if reqPath[i] == '/' {
 			if um.Store[reqPath[:(i+1)]] != nil {
@@ -104,7 +119,7 @@ func (um *UrlMap) getValue(reqPath string) *RouterRet {
 				break
 			} else {
 				if jk != i {
-					vals := make([]string, 0)
+					vals := make([]string, 1)
 					vals = append(vals, reqPath[i:jk])
 					values = append(vals, values...)
 					jk = i
@@ -122,7 +137,7 @@ func (um *UrlMap) getValue(reqPath string) *RouterRet {
 	param.Path = reqPath
 	param.RealPath = key
 	for _, nod := range nods {
-		if len(nod.ParamSlice) == len(values) {
+		if len(nod.ParamSlice) == len(values) || strings.Contains(nod.Type, method) {
 			param.Handle = nod.Handle
 			param.Type = nod.Type
 			param.ParamSlice = values
@@ -131,4 +146,19 @@ func (um *UrlMap) getValue(reqPath string) *RouterRet {
 	}
 
 	return param
+}
+
+func (ret *RouterRet) ByName(name string) (string, bool) {
+	var index, ok = ret.ParamMap[name]
+	if !ok {
+		return "", false
+	}
+	return ret.By(index)
+}
+
+func (ret *RouterRet) By(id uint) (string, bool) {
+	if uint(len(ret.ParamSlice)) <= id {
+		return "", false
+	}
+	return ret.ParamSlice[id], true
 }
